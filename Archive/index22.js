f68a46894b236bca212b9e54bed174b3f1008030
barendan <Barendan@gmail.com>
2016-08-19 20:26:33 -0400

    First attempt at making web page responsive to browser window resize

--- Archive/index22.js
+++ Archive/index22.js
@@ -1,8 +1,15 @@
 
-function start() {
 
 
+$(document).ready(function() {
+    $(window).resize(function() {
+        var bodyheight = $(this).height();
+        $(".m1").height(bodyheight);
+        
+    console.log( "here:", bodyheight);
 
+    }).resize();
 
-    
-}
+    console.log( $(window).width() );
+    console.log( $(window).height() );
+});
