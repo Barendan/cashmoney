89feb7a5760c99212188f33c4aeb1387442ed73d
barendan <Barendan@gmail.com>
2016-08-20 21:10:33 -0400

    Halfway complete with responsive design for all screen sizes

---
+++ index22.js
@@ -0,0 +1,27 @@
+
+
+
+// $(document).ready(function() {
+    // $(window).resize(function() {
+    //     var bodyheight = $(this).height();
+    //     $(".m1").height(bodyheight);
+
+    // console.log( "here:", bodyheight);
+
+    // }).resize();
+
+    // console.log( $(window).width() );
+    // console.log( $(window).height() );
+
+
+    // $(window).resize(function() {
+    //     var bodyheight = $(this).height();
+    //     $(".m1").height(bodyheight);
+
+    // console.log( "here:", bodyheight);
+
+    // }).resize();
+
+// });
+
+
