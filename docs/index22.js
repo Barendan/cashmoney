6b35c73455d3c998198efc99bf24c20c2f093548
barendan <Barendan@gmail.com>
2016-09-02 13:55:10 -0400

    Preparing to host

--- docs/index22.js
+++ docs/index22.js
@@ -23,6 +23,67 @@
 // });
 
 
+$('.m1Text ul li').hide().each(function(index) {
+//   $(this).slideDown(1700).delay(1000 * index);
+  // $(this).slideUp(1700).delay(1000 * index);
+  $(this).fadeIn(1700);
+});
+
+
+// $('.flake').removeClass('.flake');
+
+
+var $sB = $('.m1Text ul li');
+
+$sB.hover( 
+  function() {
+    var $svg = $(this).children().find('#icons');
+    var $flake = $(this).children().find('.flake');
+    console.log("Hovered: " + $flake);
+
+    // $flake.css("border-radius","0%");
+    $flake.hide();
+    // $svg.css("fill", "#000");
+    // $(this).fadeTo("fast", 1.0);
+  },
+  function() {
+    var $svg = $(this).children().find('#icons');
+    var $flake = $(this).children('flake');
+
+
+    $(".m1Text ul li a").css("border-radius","50%");
+    $flake.hide();
+
+    // $svg.css("fill", "#FFF");
+    // $(this).fadeTo("fast", 0.3);
+  }
+);
+
+
+
+$(document).ready(function() {
+
+    $( "#js-expand" ).click(function (){
+            // $(".jects").html(full);
+            $("h2").css("margin-bottom", "0%");
+            $(".overlay").removeClass("normal");
+            $(".overlay").addClass("full");
+    
+    });
+    
+    $( ".xl" ).click(function (){
+            $(".overlay").removeClass("full");
+            $(".overlay").addClass("normal");
+        
+    });
+});
+    
+
+
+
+
+
+
 
 var camera, scene, renderer;
 
@@ -181,107 +242,18 @@ function update() {
 
 // ------------------------------------------------
 
-var flashy = 
-`
-<svg version="1.1" id="Ebene_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
-    width="600px" height="100px" viewBox="0 0 820 95">
 
-    <defs>
-      <filter id="filter">
-        <feFlood flood-color="#043561" result="black" />
-        <feFlood flood-color="red" result="flood1" />
-        <feFlood flood-color="green" result="flood2" />
-        <feOffset in="SourceGraphic" dx="3" dy="0" result="off1a"/>
-        <feOffset in="SourceGraphic" dx="2" dy="0" result="off1b"/>
-        <feOffset in="SourceGraphic" dx="-3" dy="0" result="off2a"/>
-        <feOffset in="SourceGraphic" dx="-2" dy="0" result="off2b"/>
-        <feComposite in="flood1" in2="off1a" operator="in"  result="comp1" />
-        <feComposite in="flood2" in2="off2a" operator="in" result="comp2" />
+// $( window ).resize(function() {
 
-        <feMerge x="0" width="100%" result="merge1">
-            <feMergeNode in = "black" />
-            <feMergeNode in = "comp1" />
-            <feMergeNode in = "off1b" />
+//     if (window.innerWidth < 700) {
+//         // scripts
+//         $('.title').text(modest);
+//     }
+//     if (window.innerWidth > 700) {
+//         $('.title').html(flashy);
+//     }
 
-            <animate 
-                attributeName="y" 
-                id = "y"
-                dur ="4s"
-                
-                values = '104px; 104px; 30px; 105px; 30px; 2px; 2px; 50px; 40px; 105px; 105px; 20px; 6ßpx; 40px; 104px; 40px; 70px; 10px; 30px; 104px; 102px'
-
-                keyTimes = '0; 0.362; 0.368; 0.421; 0.440; 0.477; 0.518; 0.564; 0.593; 0.613; 0.644; 0.693; 0.721; 0.736; 0.772; 0.818; 0.844; 0.894; 0.925; 0.939; 1'
-
-                repeatCount = "indefinite" />
-
-            <animate attributeName="height" 
-                id = "h" 
-                dur ="4s"
-                
-                values = '10px; 0px; 10px; 30px; 50px; 0px; 10px; 0px; 0px; 0px; 10px; 50px; 40px; 0px; 0px; 0px; 40px; 30px; 10px; 0px; 50px'
-
-                keyTimes = '0; 0.362; 0.368; 0.421; 0.440; 0.477; 0.518; 0.564; 0.593; 0.613; 0.644; 0.693; 0.721; 0.736; 0.772; 0.818; 0.844; 0.894; 0.925; 0.939; 1'
-
-                repeatCount = "indefinite" />
-        </feMerge>
-        
-
-        <feMerge x="0" width="100%" y="60px" height="65px" result="merge2">
-            <feMergeNode in = "black" />
-            <feMergeNode in = "comp2" />
-            <feMergeNode in = "off2b" />
-
-            <animate attributeName="y" 
-                id = "y"
-                dur ="4s"
-                values = '103px; 104px; 69px; 53px; 42px; 104px; 78px; 89px; 96px; 100px; 67px; 50px; 96px; 66px; 88px; 42px; 13px; 100px; 100px; 104px;' 
-
-                keyTimes = '0; 0.055; 0.100; 0.125; 0.159; 0.182; 0.202; 0.236; 0.268; 0.326; 0.357; 0.400; 0.408; 0.461; 0.493; 0.513; 0.548; 0.577; 0.613; 1'
-
-                repeatCount = "indefinite" />
-
-            <animate attributeName="height" 
-                id = "h"
-                dur = "4s"
-                
-                values = '0px; 0px; 0px; 16px; 16px; 12px; 12px; 0px; 0px; 5px; 10px; 22px; 33px; 11px; 0px; 0px; 10px'
-
-                keyTimes = '0; 0.055; 0.100; 0.125; 0.159; 0.182; 0.202; 0.236; 0.268; 0.326; 0.357; 0.400; 0.408; 0.461; 0.493; 0.513;  1'
-                 
-                repeatCount = "indefinite" />
-        </feMerge>
-            
-            <feMerge>
-                <feMergeNode in="SourceGraphic" />  
-
-                <feMergeNode in="merge1" /> 
-            <feMergeNode in="merge2" />
-
-            </feMerge>
-        </filter>
-
-    </defs>
-
-    <g>
-    <text x="0" y="110">Daniel Barenboim</text>
-    </g>
-
-</svg>
-`
-var modest = `Daniel Barenboim`
-
-
-$( window ).resize(function() {
-
-    if (window.innerWidth < 700) {
-        // scripts
-        $('.title').text(modest);
-    }
-    if (window.innerWidth > 700) {
-        $('.title').html(flashy);
-    }
-
-});
+// });
 
 
 
@@ -304,22 +276,7 @@ var full = `
 <img class="xl" src="images/FireWater.png" width="100%" height="100%">
             `;
 
-$(document).ready(function() {
 
-    $( "#js-expand" ).click(function (){
-            // $(".jects").html(full);
-            $("h2").css("margin-bottom", "0%");
-            $(".overlay").removeClass("normal");
-            $(".overlay").addClass("full");
-    
-    });
-    
-    $( ".xl" ).click(function (){
-            $(".overlay").removeClass("full");
-            $(".overlay").addClass("normal");
-        
-    });
-});
 
 
 
