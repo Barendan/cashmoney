522ae1614ef5399f225969fb9f53e9226c0e88f0
barendan <Barendan@gmail.com>
2016-09-02 14:23:03 -0400

    added delay to let it snow

--- docs/let_it_snow.js
+++ docs/let_it_snow.js
@@ -211,34 +211,34 @@
 }(window.jQuery);
 
 
-// ---------------------------------------------------
 
 
 
 
 
 $(document).ready( function() {
-   // $("canvas.flare").let_it_snow({
-   //    windPower: 0,
-   //    speed: 0,
-   //    color: "#392F52",
-   //    size:120,
-   //    opacity: 0.00000001,
-   //    count: 40,
-   //    interaction: false
-   //  });
-    $("canvas.snow").let_it_snow({
+   $("canvas.flare").hide().fadeIn(15000).let_it_snow({
+      windPower: 0,
+      speed: 0,
+      color: "#392F52",
+      size:120,
+      opacity: 0.00000001,
+      count: 40,
+      interaction: false
+    });
+    $("canvas.snow").hide().fadeIn(5000).let_it_snow({
         windPower: 3,
         speed: 1,
         count: 250,
         size: 0,
     });
-    $("canvas.flake").let_it_snow({
+    $("canvas.flake").hide().delay(3000).fadeIn(1500).let_it_snow({
         windPower: -3,
         speed: 1,
         count: 20,
         size: 10,
         image: "images/white-snowflake.png",
+        interaction: true
     });
 
 
